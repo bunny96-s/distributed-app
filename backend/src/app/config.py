19bc81aab9ab2204185968a0684ad2050204c12f
mariababu5 <74399458+mariababu5@users.noreply.github.com>
2025-03-10 19:04:58 -0400

    Update config.py

--- backend/src/app/config.py
+++ backend/src/app/config.py
@@ -1,7 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-# Get DATABASE_URL from environment variable or use default
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://kishan98:testdb@db:5432/app_db")
